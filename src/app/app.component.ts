276a0a81b619d2f5711dcd9ad424a95c0fa73442
sevriukovmk <sevriukovmk@gmail.com>
2020-09-06 18:32:12 +0300

    [ATLAS-8] Create AtlasComponent, add log in button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {KeycloakService} from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'atlas-frontend';
+export class AppComponent implements OnInit{
+  constructor(private ks: KeycloakService) {
+  }
+
+  ngOnInit(): void {
+    if (this.ks.isTokenExpired()) {
+
+    }
+  }
 }
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {KeycloakService} from 'keycloak-angular';
 import {KeycloakProfile} from 'keycloak-js';
+import { Octokit } from '@octokit/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,17 +12,15 @@ import {KeycloakProfile} from 'keycloak-js';
 export class ToolbarComponent implements OnInit {
 
   profile: KeycloakProfile;
+  githubToken = '';
   isLoggedIn = false;
 
 
-  constructor(private ks: KeycloakService) { }
+  constructor(private ks: KeycloakService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.ks.isLoggedIn().then(value => {
       this.isLoggedIn = value;
-    });
-    this.ks.loadUserProfile().then( profile => {
-      this.profile = profile;
     });
   }
 
@@ -31,4 +31,30 @@ export class ToolbarComponent implements OnInit {
   logIn(): void {
     this.ks.login().then();
   }
+
+  getUser(): void {
+    this.ks.getToken().then(token => {
+      console.log('token', token);
+      let headers = new HttpHeaders();
+      headers = headers.set('Authorization', `Bearer ${token}`);
+      this.http.get(`http://localhost:8080/auth/realms/Atlas.local/broker/github/token`, {headers, responseType: 'blob'}).subscribe(t => {
+
+        const fr = new FileReader();
+
+        fr.onload = () => {
+          const result = fr.result.toString();
+          const strings = result.split('&');
+          const tokens = strings[0].split('=');
+          this.githubToken = tokens[1];
+        };
+        fr.readAsText(t);
+
+        const octokit = new Octokit({
+          auth: this.githubToken
+        });
+        octokit.request(`/user`).then(user => console.log('user', user));
+      });
+    });
+  }
 }
+
