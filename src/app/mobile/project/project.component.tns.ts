e13cbdb55b263e90525f1b098c8804b42aae7abc
Oksana Kulbaba <o.coolbaba@gmail.com>
2021-06-06 18:46:34 +0300

    add get project

--- src/app/mobile/project/project.component.tns.ts
+++ src/app/mobile/project/project.component.tns.ts
@@ -1,26 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import {Application, ItemEventData} from '@nativescript/core';
+import {Organization, Project} from '@src/app/shared/atlas/entity.service';
+import {from, Observable, Subscription, zip} from 'rxjs';
+import {ProjectService} from '@src/app/web/project/services/project.service';
+import {OrganizationService} from '@src/app/services/organization.service';
+import {switchMap} from 'rxjs/operators';
+import {UserClaims} from '@okta/okta-auth-js/lib/types';
 
 @Component({
   moduleId: module.id,
-  selector: 'Project',
+  selector: 'app-Project-ng',
   templateUrl: './project.component.tns.html',
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements OnInit, OnDestroy {
 
-  projects: { name: string, imageSrc: string }[] = [
-    { name: 'New Prog', imageSrc: 'res://donut' },
+  organizations: Organization[] = [];
+  projects: Project[] = [];
+  private $usersProjects: Subscription = Subscription.EMPTY;
+  private $usersOrganizations: Subscription = Subscription.EMPTY;
+
+  user: {
+    userId: '00u2v5jxvoGXWqQTw4x7'
+  };
+  loading = true;
+
+  constructor(
+    private organizationService: OrganizationService,
+    private projectService: ProjectService,
+  ) {
+    this.$usersOrganizations = this.organizationService.findAllByUserId('00u2v5jxvoGXWqQTw4x7')
+      .subscribe((organizations) => {
+
+        this.organizations = organizations;
+        this.loading = false;
+
+      }, error => this.loading = false);
+    this.$usersProjects = this.projectService.findAllByUserId('00u2v5jxvoGXWqQTw4x7')
+      .subscribe((projects) => {
+        this.projects = projects;
+        this.loading = false;
+      }, error => this.loading = false);
+    this.loading = true;
 
 
-    { name: 'New Prog', imageSrc: 'res://donut' },
-    { name: 'New Prog', imageSrc: 'res://donut' },
-    { name: 'New Prog', imageSrc: 'res://donut' },
-    { name: 'New Prog', imageSrc: 'res://donut' },
-    { name: 'New Prog', imageSrc: 'res://donut' },
-  ];
-  constructor() {
-    // Use the component constructor to inject providers.
   }
 
   ngOnInit(): void {
@@ -30,8 +53,18 @@ export class ProjectComponent implements OnInit {
     console.log('Item with index: ' + args.index + ' tapped');
   }
 
-/*  onDrawerButtonTap(): void {
-    const sideDrawer = <RadSideDrawer>Application.getRootView()
-    sideDrawer.showDrawer()
-  }*/
+  ngOnDestroy(): void {
+    this.$usersOrganizations.unsubscribe();
+    this.$usersProjects.unsubscribe();
+  }
+
+  getOrganizationByProject(project: Project): Organization {
+    console.log(this.organizations);
+    return this.organizations.filter(org => org.id === project.organizationId)[0];
+  }
+
+  /*  onDrawerButtonTap(): void {
+      const sideDrawer = <RadSideDrawer>Application.getRootView()
+      sideDrawer.showDrawer()
+    }*/
 }
